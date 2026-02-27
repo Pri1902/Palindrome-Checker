4e2b028aa97a5576f809d5cb009afb1bb9faa371
Pri1902 <Priyankapraveenajain@gmail.com>
2026-02-27 10:46:49 +0530

    UC7-Deque

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,21 +1,22 @@
 public class PalindromeChecker{
-    public static boolean queueStackCheck(String original) {
-        java.util.Queue<Character> queue = new java.util.LinkedList<>();
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-        for (char ch : original.toCharArray()) {
-            queue.add(ch);
-            stack.push(ch);
-        }
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop()))
+    public static boolean dequeCheck(String original) {
+
+        java.util.Deque<Character> deque = new java.util.ArrayDeque<>();
+
+        for (char ch : original.toCharArray())
+            deque.add(ch);
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast()))
                 return false;
         }
         return true;
     }
+
     public static void main(String[] args) {
         String original = "madam";
         System.out.println("Input Text: " + original);
-        System.out.println("Is it a palindrome: " + queueStackCheck(original));
+        System.out.println("Is it a palindrome: " + dequeCheck(original));
     }
 }
 
