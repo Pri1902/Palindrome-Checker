2fb68de342254b486548be224469fa6557caa2e1
Pri1902 <Priyankapraveenajain@gmail.com>
2026-02-27 09:38:06 +0530

    UC1-Welcome Message

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,5 +1,7 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-
+        System.out.println(" Welcome to Palindrome Checker APP ");
+        System.out.println(" App Version : 1.1.1 ");
+        System.out.println(" System initialized successfully ");
     }
 }
