df8c08bb88c414023252accf137c5dc90acbe7f7
Pri1902 <Priyankapraveenajain@gmail.com>
2026-02-27 09:57:09 +0530

    UC4-Character array

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,17 +1,20 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
         String original = "madam";
-        String reversed = "";
         System.out.println("Input Text: " + original);
+        char[] chars = original.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
 
-        for(int i = original.length() - 1; i >= 0; --i) {
-            reversed = reversed + original.charAt(i);
+        boolean isPalindrome;
+        for(isPalindrome = true; start < end; --end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+            }
+
+            ++start;
         }
 
-        if (original.equals(reversed)) {
-            System.out.println("Is it a palindrome: True");
-        } else {
-            System.out.println("Is it a palindrome: False");
-        }
+        System.out.println("Is it a palindrome: " + isPalindrome);
     }
 }
