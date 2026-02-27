0fe60742cf8bc2e68eadf4101fd9ab7c6ede6896
Pri1902 <Priyankapraveenajain@gmail.com>
2026-02-27 11:00:39 +0530

    UC8-Linked List

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,22 +1,62 @@
 public class PalindromeChecker{
-    public static boolean dequeCheck(String original) {
+    static class Node {
+        char data;
+        Node next;
 
-        java.util.Deque<Character> deque = new java.util.ArrayDeque<>();
-
-        for (char ch : original.toCharArray())
-            deque.add(ch);
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast()))
-                return false;
+        Node(char data) {
+            this.data = data;
         }
+    }
+
+    public static boolean linkedListCheck(String original) {
+
+        Node head = null;
+        Node tail = null;
+
+        for (char ch : original.toCharArray()) {
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        while (slow != null) {
+            Node next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        Node first = head;
+        Node second = prev;
+
+        while (second != null) {
+            if (first.data != second.data)
+                return false;
+
+            first = first.next;
+            second = second.next;
+        }
+
         return true;
     }
 
     public static void main(String[] args) {
         String original = "madam";
         System.out.println("Input Text: " + original);
-        System.out.println("Is it a palindrome: " + dequeCheck(original));
+        System.out.println("Is it a palindrome: " + linkedListCheck(original));
     }
 }
 
