a94be7708f4ff70057d5963e2224d83b113cb962
Pri1902 <Priyankapraveenajain1902@gmail.com>
2026-03-16 09:38:58 +0530

    UC9: Recursive Palindrome Checker

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,5 +1,18 @@
 public class PalindromeChecker {
+    public static boolean recursiveCheck(String str, int start, int end) {
+
+        if (start >= end)
+            return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return recursiveCheck(str, start + 1, end - 1);
+    }
     public static void main(String[] args) {
+        String original = "madam";
+        System.out.println("Input Text: " + original);
+        System.out.println("Is it a palindrome: " + recursiveCheck(original, 0, original.length() - 1));
 
     }
 }
