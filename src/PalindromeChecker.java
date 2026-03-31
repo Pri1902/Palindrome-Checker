55312ee1efce06e81ebfafaf950f8a16ca9bb357
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-01 02:53:27 +0530

    UC11:OOPS Concept

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,26 +1,25 @@
 
 public class PalindromeChecker{
-     public static boolean normalizedCheck(String input) {
+        public static boolean checkPalindrome(String input) {
 
-        String clean = input.replaceAll("\\s+", "").toLowerCase();
+            int left = 0;
+            int right = input.length() - 1;
 
-        int left = 0;
-        int right = clean.length() - 1;
+            while (left < right) {
+                if (input.charAt(left) != input.charAt(right))
+                    return false;
 
-        while (left < right) {
-            if (clean.charAt(left) != clean.charAt(right))
-                return false;
-            left++;
-            right--;
+                left++;
+                right--;
+            }
+
+            return true;
         }
-
-        return true;
-    }
 
     public static void main(String[] args) {
         String original = "madam";
         System.out.println("Input Text: " + original);
-        System.out.println("Is it a palindrome: " + normalizedCheck(original));
+        System.out.println("Is it a palindrome: " + checkPalindrome(original));
     }
 }
 
