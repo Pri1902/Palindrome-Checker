2e315386450e008fcc083108bd43df4797979ba3
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-01 02:43:26 +0530

    UC10:Case & Space Ignored

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,53 +1,17 @@
+
 public class PalindromeChecker{
-    static class Node {
-        char data;
-        Node next;
+     public static boolean normalizedCheck(String input) {
 
-        Node(char data) {
-            this.data = data;
-        }
-    }
+        String clean = input.replaceAll("\\s+", "").toLowerCase();
 
-    public static boolean linkedListCheck(String original) {
+        int left = 0;
+        int right = clean.length() - 1;
 
-        Node head = null;
-        Node tail = null;
-
-        for (char ch : original.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node prev = null;
-        while (slow != null) {
-            Node next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-
-        Node first = head;
-        Node second = prev;
-
-        while (second != null) {
-            if (first.data != second.data)
+        while (left < right) {
+            if (clean.charAt(left) != clean.charAt(right))
                 return false;
-
-            first = first.next;
-            second = second.next;
+            left++;
+            right--;
         }
 
         return true;
@@ -56,7 +20,7 @@ public class PalindromeChecker{
     public static void main(String[] args) {
         String original = "madam";
         System.out.println("Input Text: " + original);
-        System.out.println("Is it a palindrome: " + linkedListCheck(original));
+        System.out.println("Is it a palindrome: " + normalizedCheck(original));
     }
 }
 
