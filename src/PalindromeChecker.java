ce48b0dd55cdae75b9de66f4750d2667e1a40a1b
Pri1902 <priyankapraveenajain1902@gmail.com>
2026-04-01 03:23:13 +0530

    UC13: Performance Comparison

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,11 +1,21 @@
+public class PalindromeChecker {
 
-public class PalindromeChecker{
-    interface PalindromeStrategy {
-        boolean check(String input);
+    // Array Method
+    public static boolean arrayCheck(String input) {
+        char[] arr = input.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] != arr[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 
-        public static boolean stackCheck(String input) {
-
+    // Stack Method
+    public static boolean stackCheck(String input) {
         java.util.Stack<Character> stack = new java.util.Stack<>();
 
         for (char ch : input.toCharArray())
@@ -18,49 +28,98 @@ public class PalindromeChecker{
         return true;
     }
 
-    static class StackStrategy implements PalindromeStrategy {
+    // Recursive Method
+    public static boolean recursiveCheck(String str, int start, int end) {
+        if (start >= end)
+            return true;
 
-        public boolean check(String input) {
-            return stackCheck(input);
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return recursiveCheck(str, start + 1, end - 1);
+    }
+
+    // Linked List Node
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
         }
     }
 
-        public static boolean dequeCheck(String input) {
+    // Linked List Method
+    public static boolean linkedListCheck(String input) {
+        Node head = null, tail = null;
 
-        java.util.Deque<Character> deque = new java.util.ArrayDeque<>();
+        for (char ch : input.toCharArray()) {
+            Node newNode = new Node(ch);
+            if (head == null)
+                head = tail = newNode;
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
 
-        for (char ch : input.toCharArray())
-            deque.add(ch);
+        Node slow = head, fast = head;
 
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast()))
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        while (slow != null) {
+            Node next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        Node first = head, second = prev;
+
+        while (second != null) {
+            if (first.data != second.data)
                 return false;
+
+            first = first.next;
+            second = second.next;
         }
 
         return true;
     }
 
-    static class DequeStrategy implements PalindromeStrategy {
+    // Performance Test
+    public static void performanceTest(String input) {
+        long start, end;
 
-        public boolean check(String input) {
-            return dequeCheck(input);
-        }
+        start = System.nanoTime();
+        arrayCheck(input);
+        end = System.nanoTime();
+        System.out.println("Array Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        stackCheck(input);
+        end = System.nanoTime();
+        System.out.println("Stack Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        recursiveCheck(input, 0, input.length() - 1);
+        end = System.nanoTime();
+        System.out.println("Recursion Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        linkedListCheck(input);
+        end = System.nanoTime();
+        System.out.println("LinkedList Time: " + (end - start) + " ns");
     }
 
     public static void main(String[] args) {
         String word = "madam";
-        PalindromeStrategy strategy;
-
-        strategy = new StackStrategy();
-        System.out.println(word + (strategy.check(word) ?
-                " is a Palindrome (Stack Strategy)" :
-                " is NOT a Palindrome (Stack Strategy)"));
-
-        strategy = new DequeStrategy();
-        System.out.println(word + (strategy.check(word) ?
-                " is a Palindrome (Deque Strategy)" :
-                " is NOT a Palindrome (Deque Strategy)"));
-
+        System.out.println("\n--- Performance Comparison ---");
+        performanceTest(word);
     }
 }
 
